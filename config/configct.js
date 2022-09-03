a0dce60096bb31a6ef8535e79c59f0d2d2d8b01b
Jaipradhap <jaipradhap.ma@gmail.com>
2022-09-03 15:01:10 +0400

    testnet

--- config/configct.js
+++ config/configct.js
@@ -1,4 +1,4 @@
-export const CONTADDRESS = '0x4Be25b76C136D02E971E8c9c08520865c158438e'; // Contract Address
+export const CONTADDRESS = '0x07311F4e158F1e29FF5e1dFf61256501591EE1F3'; // Contract Address
 export const TOKENADDRESS = '0x28F849a7800a5BD3f6416Dd13C0AcCaCDC7Cf840'; // FASTX Token
 // export const TXNURL = 'https://polygonscan.com/tx/';
 export const TXNURL = 'https://mumbai.polygonscan.com/tx/';
@@ -12,14 +12,8 @@ export const DSTATECT = 'https://mumbai.polygonscan.com/address/'.concat(CONTADD
 export const TOKENCT = 'https://mumbai.polygonscan.com/address/'.concat(TOKENADDRESS);
 
 
-
-// Urls
-export const PAPERS = 'https://rabbiteggsdefi.github.io/maticguide/Rabbit_Eggs_Defi_Whitepaper.pdf';
-export const GUIDE = 'https://rabbiteggsdefi.github.io/maticguide/';
-export const TIPS = 'https://rabbiteggsdefi.github.io/maticguide/#tipstricks';
-export const BSCTESTNET = 'https://rabbiteggsdefi.github.io/polygontestnetnfts/';
-
 // Social Media Links
 export const TWITTER ='https://twitter.com/Defieggs';
-export const TIKTOK ='https://discord.com/invite/4Nqw6RN8nU';
+export const INSTA = 'https://www.instagram.com/rabbiteggsdefi';
+export const MEDIUM ='https://discord.com/invite/4Nqw6RN8nU';
 export const YOUTUBE ='https://www.youtube.com/channel/UCGaNXVdMp59672Nkjl3GsVg';
--- src/layouts/SidebarLayout/Header/Buttons/index.js
+++ src/layouts/SidebarLayout/Header/Buttons/index.js
@@ -2,19 +2,20 @@ import { Box , Tooltip, IconButton } from '@mui/material';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import HeaderNotifications from './Notifications';
+import { TWITTER , MEDIUM , INSTA } from 'config/configct';
 
 function HeaderButtons() {
   return (
     <Box sx={{ mr: 1 }}>
       {/* <HeaderSearch /> */}
       <Tooltip arrow placement="top" title="Instagram">
-                    <IconButton color="primary" onClick={event =>  window.open('https://www.instagram.com/rabbiteggsdefi')}>
+                    <IconButton color="primary" onClick={event =>  window.open(INSTA)}>
                       <InstagramIcon />
                     </IconButton>
                   </Tooltip>
 
       <Tooltip arrow placement="top" title="Twitter" >
-                  <IconButton color="primary" onClick={event =>  window.open('https://twitter.com/DefiEggs')}>
+                  <IconButton color="primary" onClick={event =>  window.open(TWITTER)}>
                     <TwitterIcon />
                   </IconButton>
                 </Tooltip>
